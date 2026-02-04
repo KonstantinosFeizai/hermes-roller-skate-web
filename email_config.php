b50e7f323abc3d26647c6da955ba6026b4ff2409
Konstantinos Feizai <KonstantinosFeizai@outlook.com>
2026-02-04 17:51:56 +0200

    commenting

--- email_config.php
+++ email_config.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Δημιουργία αντικειμένου PHPMailer (true = περνάει εξαιρέσεις)
 function getMailer()
 {
-    $mail = new PHPMailer(true);
+    // 1. $mail = new PHPMailer(true);
 
     // Ρυθμίσεις SMTP (Αυτές θα αλλάξουν όταν ανέβετε στο Plesk/Live)
     $mail->isSMTP();
@@ -18,7 +18,7 @@ function getMailer()
     $mail->SMTPAuth = true;
     $mail->Username = 'smtp-username'; // Βάλτε το username του SMTP
     $mail->Password = 'smtp-password'; // Βάλτε τον κωδικό εφαρμογής (όχι τον κανονικό κωδικό!)
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // STARTTLS ή SMTPS ανάλογα με τον server
+    // 2. $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // STARTTLS ή SMTPS ανάλογα με τον server
     $mail->Port = 587; // 587 για STARTTLS, 465 για SMTPS
 
     // Γενικές ρυθμίσεις
